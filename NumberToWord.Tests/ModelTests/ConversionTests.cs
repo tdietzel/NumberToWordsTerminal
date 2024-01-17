1d8e28850363244ac3154e05894c28ab4f4181a5
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-01-17 09:37:04 -0800

    Add Complete Test #2
    Co-authored-by: Monica Barboza <MonBoza@gmail.com>
    Co-authored-by: Zuri Gallegos
    <Zurisadailopezmartinez@gmail.com>

--- NumberToWord.Tests/ModelTests/ConversionTests.cs
+++ NumberToWord.Tests/ModelTests/ConversionTests.cs
@@ -9,8 +9,16 @@ namespace NumberToWords.Tests
     [TestMethod]
     public void ConversionConstructor_CreatesInstanceOfConversion_Conversion()
     {
-      Conversion newConversion = new Conversion();
+      Conversion newConversion = new Conversion(357);
       Assert.AreEqual(typeof(Conversion), newConversion.GetType());
     }
-  }
+    [TestMethod]
+    public void GetConversionNum_ReturnsConversionNum_Int()
+    {
+      int userNum = 357;
+      Conversion newConversion = new Conversion(userNum);
+      int result = newConversion.ConversionNum;
+      Assert.AreEqual(userNum, result);
+    }
+}
 }
--- NumberToWord/Models/Conversion.cs
+++ NumberToWord/Models/Conversion.cs
@@ -1,7 +1,14 @@
+using NumberToWords.Models;
+
 namespace NumberToWords.Models
 {
   public class Conversion
   {
-    
-  }
+    public int ConversionNum {get;}
+
+    public Conversion(int userNum)
+    {
+      ConversionNum = userNum;
+    }
+  } 
 }
