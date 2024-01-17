c6a388000512c8347886cd5228cc643b57015e92
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-01-17 14:42:09 -0800

    Add complete UI with goodbye banner
    Co-authored-by: Monica Barboza <MonBoza@gmail.com>
    Co-authored-by: Zuri Gallegos
    <Zurisadailopezmartinez@gmail.com>

--- NumberToWord/Program.cs
+++ NumberToWord/Program.cs
@@ -13,15 +13,31 @@ namespace NumberToWord
       Console.ForegroundColor = ConsoleColor.White;
       Console.WriteLine(NumberToWord.UserInterfaceModels.WelcomeBanner.Welcome);
       // Console.WriteLine("Welcome to the Numbers To Words App!");
+      Conversion();
+    }
+    static void Conversion()
+    {
       Console.WriteLine("Please enter a number to convert to a word:");
-      Console.WriteLine("--------------");
+      Console.WriteLine("------------------");
       string userNum = Console.ReadLine();
       Conversion newConversion = new Conversion(int.Parse(userNum));
       string result = newConversion.Dictionary(int.Parse(userNum));
       Console.WriteLine(result);
       Console.Beep();
-      
+      Navigate();
     }
-    static void 
+    static void Navigate()
+    {
+      Console.WriteLine("Would you like another number? Enter 'yes to proceed, or 'no' to exit");
+      string userInput = Console.ReadLine();
+      if (userInput == "yes" || userInput == "Yes")
+      {
+        Conversion();
+      }
+      else if (userInput == "no" || userInput == "No")
+      {
+        Console.WriteLine(NumberToWord.UserInterfaceModels.GoodbyeBanner.Goodbye);
+      }
+    }
   }
 }
