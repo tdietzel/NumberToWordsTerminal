99af767f0b9455889c52b149789deedd31794488
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-01-17 11:55:23 -0800

    Refactor code to only use each Dictionary once

--- NumberToWord/Models/Conversion.cs
+++ NumberToWord/Models/Conversion.cs
@@ -18,21 +18,22 @@ namespace NumberToWords.Models
       return digitArray.Length;
     }
     public string Dictionary(int userNum) {
+      Dictionary<int, string> Hundreds = new Dictionary<int, string>(){
+        {1, "one hundred"}, {2, "two hundred"}, {3,"three hundred"}, {4, "four hundred"}, {5, "five hundred"}, {6, "six hundred"}, {7, "seven hundred"}, {8, "eight hundred"}, {9, "nine hundred"}
+      };
+      Dictionary<int, string> Tens = new Dictionary<int, string>() {
+        {2,"twenty"}, {3,"thirty"}, {4,"forty"}, {5,"fifty"}, {6,"sixty"}, {7,"seventy"}, {8,"eighty"}, {9,"ninety"}
+      };
+      Dictionary<int, string> Teens = new Dictionary<int, string>() {
+        {0,"ten"}, {1,"eleven"}, {2,"twelve"}, {3,"thirteen"}, {4,"fourteen"}, {5,"fifteen"}, {6,"sixteen"}, {7,"seventeen"}, {8,"eighteen"}, {9,"nineteen"}
+      };
+      Dictionary<int, string> Ones = new Dictionary<int, string>() {
+        {0,"zero"}, {1,"one"}, {2,"two"}, {3,"three"}, {4,"four"}, {5,"five"}, {6,"six"}, {7,"seven"}, {8,"eight"}, {9,"nine"}
+      };
+      
       int length = CheckNumLength();
       if(length == 3)
       {
-        Dictionary<int, string> Hundreds = new Dictionary<int, string>(){
-          {1, "one hundred"}, {2, "two hundred"}, {3,"three hundred"}, {4, "four hundred"}, {5, "five hundred"}, {6, "six hundred"}, {7, "seven hundred"}, {8, "eight hundred"}, {9, "nine hundred"}
-        };
-        Dictionary<int, string> Tens = new Dictionary<int, string>() {
-          {2,"twenty"}, {3,"thirty"}, {4,"forty"}, {5,"fifty"}, {6,"sixty"}, {7,"seventy"}, {8,"eighty"}, {9,"ninety"}
-        };
-        Dictionary<int, string> Teens = new Dictionary<int, string>() {
-          {0,"ten"}, {1,"eleven"}, {2,"twelve"}, {3,"thirteen"}, {4,"fourteen"}, {5,"fifteen"}, {6,"sixteen"}, {7,"seventeen"}, {8,"eighteen"}, {9,"nineteen"}
-        };
-        Dictionary<int, string> Ones = new Dictionary<int, string>() {
-          {0,"zero"}, {1,"one"}, {2,"two"}, {3,"three"}, {4,"four"}, {5,"five"}, {6,"six"}, {7,"seven"}, {8,"eight"}, {9,"nine"}
-        };
         char[] digitArray = ConversionNum.ToString().ToCharArray();
         int firstDigit = int.Parse(digitArray[0].ToString());
         int secondDigit = int.Parse(digitArray[1].ToString());
@@ -48,15 +49,6 @@ namespace NumberToWords.Models
       }
       else if(length == 2)
       {
-        Dictionary<int, string> Tens = new Dictionary<int, string>() {
-          {2,"twenty"}, {3,"thirty"}, {4,"forty"}, {5,"fifty"}, {6,"sixty"}, {7,"seventy"}, {8,"eighty"}, {9,"ninety"}
-        };
-        Dictionary<int, string> Teens = new Dictionary<int, string>() {
-          {0,"ten"}, {1,"eleven"}, {2,"twelve"}, {3,"thirteen"}, {4,"fourteen"}, {5,"fifteen"}, {6,"sixteen"}, {7,"seventeen"}, {8,"eighteen"}, {9,"nineteen"}
-        };
-        Dictionary<int, string> Ones = new Dictionary<int, string>() {
-          {0,"zero"}, {1,"one"}, {2,"two"}, {3,"three"}, {4,"four"}, {5,"five"}, {6,"six"}, {7,"seven"}, {8,"eight"}, {9,"nine"}
-        };
         char[] digitArray = ConversionNum.ToString().ToCharArray();
         int firstDigit = int.Parse(digitArray[0].ToString());
         int secondDigit = int.Parse(digitArray[1].ToString());
@@ -71,9 +63,7 @@ namespace NumberToWords.Models
       }
       else if(length == 1)
       {
-        Dictionary <int, string> Ones = new Dictionary<int, string>() {
-          {0,"zero"}, {1,"one"}, {2,"two"}, {3,"three"}, {4,"four"}, {5,"five"}, {6,"six"}, {7,"seven"}, {8,"eight"}, {9,"nine"}
-        };
+    
         return Ones[userNum];
       }
       else
