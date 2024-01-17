4ee32159c4d3dc267c38dbd55a0ba88f1b77eb56
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-01-17 14:25:02 -0800

    Add UI with banner
    Co-authored-by: Monica Barboza <MonBoza@gmail.com>
    Co-authored-by: Zuri Gallegos
    <Zurisadailopezmartinez@gmail.com>

--- NumberToWord/Program.cs
+++ NumberToWord/Program.cs
@@ -1,12 +1,27 @@
 using System;
+using System.Collections.Generic;
+using NumberToWords.Models;
+using NumberToWord.UserInterfaceModels;
 
-namespace ExampleName
+namespace NumberToWord
 {
   class Program
   {
     static void Main()
     {
-      Console.WriteLine("Hello World!");
+      Console.BackgroundColor = ConsoleColor.Blue;
+      Console.ForegroundColor = ConsoleColor.White;
+      Console.WriteLine(NumberToWord.UserInterfaceModels.WelcomeBanner.Welcome);
+      // Console.WriteLine("Welcome to the Numbers To Words App!");
+      Console.WriteLine("Please enter a number to convert to a word:");
+      Console.WriteLine("--------------");
+      string userNum = Console.ReadLine();
+      Conversion newConversion = new Conversion(int.Parse(userNum));
+      string result = newConversion.Dictionary(int.Parse(userNum));
+      Console.WriteLine(result);
+      Console.Beep();
+      
     }
+    static void 
   }
 }
